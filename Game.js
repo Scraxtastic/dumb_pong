4c171097d2b2a698b409b3b95e19f66b425cfce7
Scraxtastic <41436549+Scraxtastic@users.noreply.github.com>
2021-02-18 23:09:26 +0100

    Pong V1 with 3 bob profiles

--- Game.js
+++ Game.js
@@ -1,18 +1,97 @@
-document.body.addEventListener('keydown', ()=>{})
-const createGame = ({ g, canvas, updateDelay, playerPlaying }) => {
+/**
+ * player1 and 2 movement (up & down)
+ * 83=s, 87=w, 38=up_arrow, 40=down_arrow
+ */
+const keyCodes = {
+  player1u: 87,
+  player1d: 83,
+  player2u: 38,
+  player2d: 40,
+};
+
+window.Game = { keyCodes };
+document.body.addEventListener("keydown", ({ keyCode }) => {
+  keyHandle({ keyCode, outcome: true });
+});
+document.body.addEventListener("keyup", ({ keyCode }) => {
+  keyHandle({ keyCode, outcome: false });
+});
+const keyHandle = ({ keyCode, outcome }) => {
+  switch (keyCode) {
+    case keyCodes.player1u:
+      window.Game.player1u = outcome;
+      break;
+    case keyCodes.player1d:
+      window.Game.player1d = outcome;
+      break;
+    case keyCodes.player2u:
+      window.Game.player2u = outcome;
+      break;
+    case keyCodes.player2d:
+      window.Game.player2d = outcome;
+      break;
+  }
+};
+const createGame = ({
+  g,
+  canvas,
+  updateDelay = 50,
+  player1Playing = false,
+  player2Playing = false,
+  botProfile1 = "bob",
+  botProfile2 = "bob",
+  bounceStrength = 10,
+  shallClear = true,
+  player1Color = "#00ff00",
+  player2Color = "#0000ff",
+}) => {
+  const speedFactor = updateDelay / 1000;
   const colors = { frame: "rgb(230,230,230)", ball: "#ff0000" };
   const state = { running: true, run: 0, interval: null };
   const canvasClientRect = canvas.getBoundingClientRect();
   const screenWidth = canvasClientRect.width;
   const screenHeight = canvasClientRect.height;
   /**
-   *
+   * GameObjects
    */
+  const ballRadius = 5;
+  const ballStartPosition = {
+    x: screenWidth / 2 - ballRadius,
+    y: screenHeight / 2 - ballRadius,
+  };
+  const ballStartSpeed = {
+    xspeed: 600,
+    yspeed: 1,
+  };
   const ball = {
-    x: screenWidth / 2,
-    y: screenHeight / 2,
-    size: 5,
-    speed: 1
+    x: ballStartPosition.x,
+    y: ballStartPosition.y,
+    size: ballRadius,
+    xspeed: ballStartSpeed.xspeed,
+    yspeed: ballStartSpeed.yspeed,
+  };
+
+  const playerConfig = {
+    height: 100,
+    width: 10,
+    speed: 300,
+    ystart: screenHeight / 2 - 100 / 2,
+  };
+  const player1 = {
+    x: 20,
+    y: playerConfig.ystart,
+    width: playerConfig.width,
+    height: playerConfig.height,
+    speed: playerConfig.speed,
+    color: player1Color,
+  };
+  const player2 = {
+    x: screenWidth - 20 - playerConfig.width,
+    y: playerConfig.ystart,
+    width: playerConfig.width,
+    height: playerConfig.height,
+    speed: playerConfig.speed,
+    color: player2Color,
   };
 
   /**
@@ -29,8 +108,11 @@ const createGame = ({ g, canvas, updateDelay, playerPlaying }) => {
    * Render functions
    */
   const render = () => {
-    renderFrame();
+    if (shallClear) g.clearRect(0, 0, screenWidth, screenHeight);
     renderBall();
+    renderPlayer(player1);
+    renderPlayer(player2);
+    renderFrame();
   };
 
   const renderFrame = () => {
@@ -39,30 +121,251 @@ const createGame = ({ g, canvas, updateDelay, playerPlaying }) => {
       screenWidth,
       screenHeight,
     });
-    g.fillRect(0, 0, screenWidth, frameHeight);
-    g.fillRect(screenWidth - frameThickness, 0, frameThickness, screenHeight);
-    g.fillRect(0, screenHeight - frameHeight, screenWidth, frameHeight);
-    g.fillRect(0, 0, frameThickness, screenWidth);
+    g.beginPath();
+    g.rect(0, 0, screenWidth, frameHeight);
+    g.rect(screenWidth - frameThickness, 0, frameThickness, screenHeight);
+    g.rect(0, screenHeight - frameHeight, screenWidth, frameHeight);
+    g.rect(0, 0, frameThickness, screenWidth);
+    g.fill();
   };
 
   const renderBall = () => {
     g.fillStyle = colors.ball;
-    const ellipse = g.ellipse(ball.x, ball.y, ball.size, ball.size, 0, 0, 180);
-    g.fill(ellipse);
+    g.beginPath();
+    g.ellipse(ball.x, ball.y, ball.size, ball.size, 0, 0, 360);
+    g.fill();
+  };
+
+  const renderPlayer = (player) => {
+    g.beginPath();
+    g.fillStyle = player.color;
+    g.rect(player.x, player.y, player.width, player.height);
+    g.fill();
   };
 
   /**
    * logic functions
    */
-  const update = () => {};
+  const run = () => {
+    if (!state.running || window.gamePaused) return;
+    update();
+    requestAnimationFrame(render);
+    state.run++;
+  };
+  const update = () => {
+    ball.x += ball.xspeed * speedFactor;
+    ball.y += ball.yspeed * speedFactor;
+    if (player1Playing) updatePlayer1();
+    else {
+      updateBot({ player: player1, botProfile: botProfile1 });
+    }
+    if (player2Playing) updatePlayer2();
+    else {
+      updateBot({ player: player2, botProfile: botProfile2 });
+    }
+    checkCollisions();
+  };
 
-  const updateBot = () => {
+  const updateBot = ({ player, botProfile }) => {
+    const ballcenterY = ball.y + ball.size;
+    const playerCenterY = player.y + player.height / 2;
+    const centerDif = Math.sqrt(Math.pow(ballcenterY - playerCenterY, 2));
+    const maxPlayerMovement = player1.speed * speedFactor;
+    switch (botProfile) {
+      case "bob":
+        updateBob({
+          player,
+          ballcenterY,
+          playerCenterY,
+          centerDif,
+          maxPlayerMovement,
+        });
+        break;
+      case "bob1":
+        updateBob1({
+          player,
+          ballcenterY,
+          playerCenterY,
+          centerDif,
+          maxPlayerMovement,
+        });
+        break;
+      case "bob2":
+        updateBob2({
+          player,
+          ballcenterY,
+          playerCenterY,
+          centerDif,
+          maxPlayerMovement,
+        });
+        break;
+    }
+  };
 
-  }
+  const updatePlayer1 = () => {
+    if (window.Game.player1u) {
+      player1.y -= player1.speed * speedFactor;
+    }
+    if (window.Game.player1d) {
+      player1.y += player1.speed * speedFactor;
+    }
+  };
 
-  const updatePlayer = () => {
+  const updatePlayer2 = () => {
+    if (window.Game.player2u) {
+      player2.y -= player2.speed * speedFactor;
+    }
+    if (window.Game.player2d) {
+      player2.y += player2.speed * speedFactor;
+    }
+  };
 
-  }
+  const resetPositions = (player1Point) => {
+    ball.x = ballStartPosition.x;
+    ball.y = ballStartPosition.y;
+    ball.xspeed = player1Point ? -ballStartSpeed.xspeed : ballStartSpeed.xspeed;
+    ball.yspeed = ballStartSpeed.yspeed;
+    player1.y = playerConfig.ystart;
+    player2.y = playerConfig.ystart;
+  };
+
+  /**
+   * Bot functions
+   */
+
+  const updateBob = ({
+    player,
+    ballcenterY,
+    playerCenterY,
+    centerDif,
+    maxPlayerMovement,
+  }) => {
+    if (ballcenterY < playerCenterY) {
+      if (centerDif > maxPlayerMovement) {
+        player.y -= player.speed * speedFactor;
+      } else {
+        player.y -= centerDif;
+      }
+    }
+    if (ballcenterY > playerCenterY) {
+      if (centerDif > maxPlayerMovement) {
+        player.y += player.speed * speedFactor;
+      } else {
+        player.y += centerDif;
+      }
+    }
+  };
+
+  const updateBob1 = ({
+    player,
+    ballcenterY,
+    centerDif,
+    maxPlayerMovement,
+  }) => {
+    const ballUpwards = ball.yspeed < 0;
+    const ballDistance = ballUpwards
+      ? Math.sqrt(Math.pow(ballcenterY - player.y + player.height, 2))
+      : Math.sqrt(Math.pow(ballcenterY - player.y, 2));
+    console.log("BALLDIST", ballDistance, maxPlayerMovement);
+    if (ballUpwards && ballcenterY < player.y + player.height) {
+      if (ballDistance > maxPlayerMovement) {
+        player.y -= maxPlayerMovement;
+      } else {
+        player.y -= ballDistance;
+      }
+    }
+    if (ballcenterY > player.y + player.height) {
+      if (ballDistance > maxPlayerMovement) {
+        player.y += maxPlayerMovement;
+      } else {
+        player.y += centerDif;
+      }
+    }
+  };
+
+  const updateBob2 = ({
+    player,
+    ballcenterY,
+    playerCenterY,
+    centerDif,
+    maxPlayerMovement,
+  }) => {
+    const ballUpwards = ball.yspeed < 0;
+    if (ballUpwards && ballcenterY < playerCenterY) {
+      if (centerDif > maxPlayerMovement) {
+        player.y -= player.speed * speedFactor;
+      } else {
+        player.y -= centerDif;
+      }
+    }
+    if (!ballUpwards && ballcenterY > playerCenterY) {
+      if (centerDif > maxPlayerMovement) {
+        player.y += player.speed * speedFactor;
+      } else {
+        player.y += centerDif;
+      }
+    }
+  };
+
+  /**
+   * Collision functions
+   */
+
+  const checkCollisions = () => {
+    if (ball.x > screenWidth) {
+      resetPositions(true);
+    } else if (ball.x < 0) {
+      resetPositions(false);
+    }
+    const { frameHeight } = calcFrameSize();
+
+    keepBallInField(frameHeight);
+    keepPlayerInField({ player: player1, frameHeight });
+    keepPlayerInField({ player: player2, frameHeight });
+    if (ball.xspeed == 0) {
+      ball.xspeed = Math.random() - 0.5 * 200;
+      ball.yspeed = Math.random() - 0.5 * 200;
+    } else if (ball.xspeed > 0) {
+      if (ballHitsPlayer(player2)) {
+        const player2yCenter = player2.y + player2.height / 2;
+        const d = player2yCenter - ball.y;
+        ball.yspeed += d * -bounceStrength;
+        ball.xspeed *= -1;
+      }
+    } else if (ball.xspeed < 0) {
+      if (ballHitsPlayer(player1)) {
+        const player1yCenter = player1.y + player1.height / 2;
+        const d = player1yCenter - ball.y;
+        ball.yspeed += d * -bounceStrength;
+        ball.xspeed *= -1;
+      }
+    }
+  };
+
+  const keepBallInField = (frameHeight) => {
+    if (
+      ball.y + ball.size + frameHeight > screenHeight ||
+      ball.y - ball.size - frameHeight < 0
+    ) {
+      ball.yspeed *= -1;
+    }
+  };
+
+  const keepPlayerInField = ({ player, frameHeight }) => {
+    const bottom = screenHeight - frameHeight;
+    if (player.y < frameHeight) player.y = frameHeight;
+    if (player.y + player.height > bottom) player.y = bottom - player.height;
+  };
+
+  const ballHitsPlayer = (player) => {
+    return (
+      ball.x + ball.size > player.x &&
+      ball.x - ball.size < player.x + player.width &&
+      ball.y + ball.size > player.y &&
+      ball.y - ball.size < player.y + player.height
+    );
+  };
+
   /**
    * Visible functions
    */
@@ -74,9 +377,7 @@ const createGame = ({ g, canvas, updateDelay, playerPlaying }) => {
 
   const start = () => {
     state.interval = setInterval(() => {
-      if (!state.running || window.gamePaused) return;
-      render();
-      state.run++;
+      run();
     }, updateDelay);
   };
 
@@ -93,6 +394,6 @@ const createGame = ({ g, canvas, updateDelay, playerPlaying }) => {
     stop,
     pauseAllGames,
     resumeAllGames,
+    run,
   };
 };
-
--- main.js
+++ main.js
@@ -1,8 +1,34 @@
 const canvas = document.getElementById("playground");
 const g = canvas.getContext("2d");
 const config = {
-  updateDelay: 100,
+  updateDelay: 30,
+  player1Playing: true,
+  player2Playing: false,
+  botProfile1: "bob2",
+  botProfile2: "bob1",
+  shallClear: false,
 };
-const game = createGame({ g, canvas, ...config });
-game.start()
+const config2 = {
+  updateDelay: 30,
+  player1Playing: false,
+  player2Playing: false,
+  botProfile1: "bob",
+  botProfile2: "bob1",
+  shallClear: false,
+  player1Color: "#443322",
+  player2Color: "#333333",
+};
+const games = [];
+games.push(createGame({ g, canvas, ...config }));
+// games.push(createGame({ g, canvas, ...config2 }));
 
+const start = (games ) => {
+  console.log("Started")
+  setInterval(() => {
+    g.clearRect(0, 0, 1280, 720);
+    games.forEach((game) => {
+      game.run();
+    });
+  }, 30);
+};
+start(games);
